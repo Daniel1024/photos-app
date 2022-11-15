34f798617644ba05999f202459ee48b4ffa3d350
Daniel Lopez <d.lopez.1740@gmail.com>
2022-11-15 15:41:10 -0300

    add to take photo logic

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotosService } from '../../services/photos.service';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html'
 })
 export class GalleryComponent implements OnInit {
+  photos: string[] = [];
 
-  constructor() { }
+  constructor(private photosService: PhotosService) {
+    this.photos = this.photosService.photos;
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  takePhoto(): void {
+    this.photosService.addNewPhoto();
   }
 
 }
